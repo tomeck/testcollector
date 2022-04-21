368e6f29e5f9aec08e8f3e849c929281b85f5b2e
tomeck <dr.teck@gmail.com>
2022-04-21 17:02:12 -0400

    got optimized version working I think

--- testcollector.go
+++ testcollector.go
@@ -153,6 +153,10 @@ func validatePredicatesForTransaction(predicates []TestCasePredicate, transactio
 
 func matchTransactionToTestCase(testCase TestCase, transactions []Transaction) (Transaction, TestStatus, error) {
 
+	var err error
+	var matchingTransaction Transaction
+	testStatus := UndefinedTestStatus
+
 	for _, transaction := range transactions {
 
 		// Check whether URL matches
@@ -168,19 +172,24 @@ func matchTransactionToTestCase(testCase TestCase, transactions []Transaction) (
 		// So far, so good - now attempt to match all predicates
 		if validatePredicatesForTransaction(testCase.Predicates, transaction) {
 
-			testStatus := UndefinedTestStatus
 			if transaction.Status == testCase.ExpectedStatus {
 				testStatus = Success
 			} else {
 				testStatus = Failure
 			}
 
-			// This transaction matches all checks, so return it
-			return transaction, testStatus, nil
+			// This transaction matches all checks, so don't need to keep looking
+			matchingTransaction = transaction
+			break
 		}
 	}
 
-	return Transaction{}, UndefinedTestStatus, errors.New("Could not find transaction to match test case " + testCase.Name)
+	// If test status is still undefined, then we did not find a matching transaction
+	if testStatus == UndefinedTestStatus {
+		err = errors.New("Could not find transaction to match test case " + testCase.Name)
+	}
+
+	return matchingTransaction, testStatus, err
 }
 
 func matchTransactionsForTestRun(testRun *TestRun, transactions []Transaction) error {
@@ -197,16 +206,19 @@ func matchTransactionsToTestRun(testRun *TestRun, transactions []Transaction) er
 
 	testRunStatus := Complete
 
+	// For each test case in the test suite associated with this run...
 	for _, testCase := range testRun.TestSuite.TestCases {
 
 		fmt.Println("Searching for transactions to match test case", testCase.Name)
 		transaction, testStatus, err := matchTransactionToTestCase(testCase, transactions)
 
 		if err == nil {
+			fmt.Println("Found matching transaction")
 			testResult := TestResult{TestRun: *testRun, TestCase: testCase, Status: testStatus, Transaction: transaction, Timestamp: time.Now()}
 			testRun.TestResults = append(testRun.TestResults, testResult)
 		} else {
 			// Did not find a transaction to match this test case
+			fmt.Println("Did not find matching transaction")
 			testRunStatus = InProgress
 		}
 	}
@@ -317,11 +329,13 @@ func collectTestRun(testRunHeaderId string) (TestRun, error) {
 	predicate4 := TestCasePredicate{Attribute: "amount.total", ExpectedValue: "200"}
 	predicate5 := TestCasePredicate{Attribute: "amount.total", ExpectedValue: "900"}
 	predicate6 := TestCasePredicate{Attribute: "amount.total", ExpectedValue: "600"}
+	predicate7 := TestCasePredicate{Attribute: "amount.total", ExpectedValue: "999"}
 	testCase1 := TestCase{Id: primitive.NewObjectID(), Name: "TestCase1", Predicates: []TestCasePredicate{predicate1, predicate2, predicate3}, ExpectedStatus: 201, Url: "/ch/payments/v1/charges"}
 	testCase2 := TestCase{Id: primitive.NewObjectID(), Name: "TestCase2", Predicates: []TestCasePredicate{predicate4}, ExpectedStatus: 500, Url: "/ch/payments/v1/charges"}
 	testCase3 := TestCase{Id: primitive.NewObjectID(), Name: "TestCase3", Predicates: []TestCasePredicate{predicate5}, ExpectedStatus: 201, Url: "/ch/payments/v1/charges"}
 	testCase4 := TestCase{Id: primitive.NewObjectID(), Name: "TestCase4", Predicates: []TestCasePredicate{predicate6}, ExpectedStatus: 201, Url: "/ch/payments/v1/charges"}
-	testSuite1 := TestSuite{Name: "TestSuite1", TestCases: []TestCase{testCase1, testCase2, testCase3, testCase4}}
+	testCase5 := TestCase{Id: primitive.NewObjectID(), Name: "TestCase5", Predicates: []TestCasePredicate{predicate7}, ExpectedStatus: 201, Url: "/ch/payments/v1/charges"}
+	testSuite1 := TestSuite{Name: "TestSuite1", TestCases: []TestCase{testCase1, testCase2, testCase3, testCase4, testCase5}}
 	testRun := TestRun{Name: "Tom Run 1", ApiKey: "apikey000001", TestRunHeaderId: testRunHeaderId, TestSuite: testSuite1, Status: InProgress, Timestamp: time.Now()}
 
 	transactions, err := findTransactionsForTestRun(testRun)
@@ -361,14 +375,20 @@ func dumpTestRunReport(testRun TestRun) {
 
 	fmt.Println("Report for Test Run>", testRun.Name)
 
-	for _, testCase := range testRun.TestSuite.TestCases {
-		testResult, err := getTestResultforTestCase(testCase, testRun)
-		if err == nil {
-			fmt.Printf("Result for Test Case `%s`: %d\n", testCase.Name, testResult.Status)
-		} else {
-			fmt.Println(err)
-		}
+	for _, testResult := range testRun.TestResults {
+		fmt.Printf("Result for Test Case `%s`: %d\n", testResult.TestCase.Name, testResult.Status)
 	}
+
+	/*
+			for _, testCase := range testRun.TestSuite.TestCases {
+			testResult, err := getTestResultforTestCase(testCase, testRun)
+			if err == nil {
+				fmt.Printf("Result for Test Case `%s`: %d\n", testCase.Name, testResult.Status)
+			} else {
+				fmt.Println(err)
+			}
+		}
+	*/
 
 	fmt.Printf("Status of this test run: %d", testRun.Status)
 
